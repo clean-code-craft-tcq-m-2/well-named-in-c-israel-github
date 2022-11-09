1135142f575bd907486f4ed478b3bc8d509ab01e
Arsiel <israhell_oz@hotmail.com>
2022-11-08 21:02:02 -0600

    Update main.c
    Removed printColorCoding function implementation

--- main.c
+++ main.c
@@ -27,27 +27,6 @@ void testPairToNumber(
     assert(pairNumber == expectedPairNumber);
 }
 
-void printColorCoding()
-{
-    ColorPair colorPair;
-	enum MajorColor major;
-    enum MinorColor minor;
-	int pairNumber = 0;
-	char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-	
-	for (major = 0; major < numberOfMajorColors; major++)
-	{
-		for (minor = 0; minor < numberOfMinorColors; minor++)
-		{
-			colorPair.majorColor = major;
-			colorPair.minorColor = minor;
-			pairNumber = GetPairNumberFromColor(&colorPair);
-			ColorPairToString(&colorPair, colorPairNames);
-			printf("%s is %d\n", colorPairNames, pairNumber);
-		}
-	}
-}
-
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
