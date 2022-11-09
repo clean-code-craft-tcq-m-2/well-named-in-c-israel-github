3035585e903f903b5c31d5d4c6468b2c17189170
Arsiel <israhell_oz@hotmail.com>
2022-11-08 21:02:44 -0600

    Update colors.h
    Added printColorCoding function implementation

--- colors.h
+++ colors.h
@@ -42,3 +42,24 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
+
+void printColorCoding()
+{
+	ColorPair colorPair;
+	enum MajorColor major;
+	enum MinorColor minor;
+	int pairNumber = 0;
+	char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+	
+	for (major = 0; major < numberOfMajorColors; major++)
+	{
+		for (minor = 0; minor < numberOfMinorColors; minor++)
+		{
+			colorPair.majorColor = major;
+			colorPair.minorColor = minor;
+			pairNumber = GetPairNumberFromColor(&colorPair);
+			ColorPairToString(&colorPair, colorPairNames);
+			printf("%s is %d\n", colorPairNames, pairNumber);
+		}
+	}
+}
